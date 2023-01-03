8b0e94a209d8ae3b92377a98bbc3947352dd46b9
Ashish Dwivedi <ashishdwivedi719@gmail.com>
2023-01-04 00:16:58 +0530

    initial commit

---
+++ frontend/src/Redux/store.js
@@ -0,0 +1,14 @@
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+import {authReducer} from './Auth/authReducer';
+import {busReducer} from './Bus/busReducer'
+
+const rootReducer = combineReducers({auth:authReducer,bus:busReducer});
+
+export const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
